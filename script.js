b12872e483021c3c1d937ddc1374956c5280e20d
callmeafaf <aichiafaf@gmail.com>
2024-09-03 10:27:13 -0230

    added the operate function that calls the right operation based on the operator

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 function add(a,b){
-    return a+b
+    return a+b;
 }
 function substract(a,b){
-    return a-b
+    return a-b;
 }
 function multiply(a,b){
-    return a*b
+    return a*b;
 }
 function divide(a,b){
-    return a/b
+    return a/b;
 }
+let firstNumber;
+let secondNumber;
+let operator;
+function operate(fn,sn,op){
+    switch(op){
+        case '+':
+            return add(fn,sn);
+            break;
+            case '-':
+                return substract(fn,sn);
+                break;
+                case '*':
+                    return multiply(fn,sn);
+                    break;
+                    case '/':
+                        return divide(fn,sn);
+                        break;
+                        default:return null;
+    }
+}
+firstNumber=3;
+secondNumber=4;
+operator='*';
+console.log(operate(firstNumber,secondNumber,operator));
